23529387c45499ac16b3ed09d0357903aa2761fc
soumikj41-netizen <soumikj41@gmail.com>
2026-02-04 20:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// Scroll animation
+const fades = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  fades.forEach(el => {
+    const pos = el.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+
+    if (pos < screen - 100) {
+      el.classList.add("show");
+    }
+  });
+});
+
+// Button actions
+function yesClick() {
+  alert("This made my day 💖");
+}
+
+function maybeClick() {
+  alert("That’s perfectly okay 🌷");
+}
+//function yesClick() {
+  //window.location.href =
+    //"https://wa.me/917872765632?text=Hey%20I%20saw%20your%20website%20and%20I’d%20love%20to%20be%20your%20Valentine%20💖";
+//}
+
+//function maybeClick() {
+  //window.location.href =
+    //"https://wa.me/7872765632?text=I%20saw%20your%20website%20🌸%20Let’s%20see%20where%20things%20go%20😊";
+//}
+function sendThoughts() {
+  const msg = document.getElementById("thankYouMsg");
+  msg.classList.add("show");
+
+  setTimeout(() => {
+    window.location.href =
+      "https://wa.me/917872765632?text=Hey%20I%20went%20through%20your%20website%20and%20here’s%20what%20I%20feel%20💭";
+  }, 1800); // delay before redirect
+}
+const bgMusic = document.getElementById("bgMusic");
+let musicStarted = false;
+
+// Start music on first interaction
+function startMusic() {
+  if (!musicStarted) {
+    bgMusic.volume = 0.25; // soft volume
+    bgMusic.play();
+    musicStarted = true;
+  }
+}
+
+// Start music on scroll or click
+window.addEventListener("scroll", startMusic);
+window.addEventListener("click", startMusic);
+
+
+
